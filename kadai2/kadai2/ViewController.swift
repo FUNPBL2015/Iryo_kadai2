352974c31303bc6c4791543aea4c0d7987b715af
b1012011 <b1013062@fun.ac.jp>
2015-05-20 17:51:30 +0900

    なにもしてない

---
+++ kadai2/kadai2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  kadai2
+//
+//  Created by 公立はこだて未来大学高度ICTコース on 2015/05/20.
+//  Copyright (c) 2015年 kyouiku. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
